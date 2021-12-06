f60156723ca914e4e4a5adbaff84862273d75707
Matheus Alecksander <projetos.matheusalecksander@gmail.com>
2021-12-06 17:58:10 -0300

    Primeiro commit, estrutura da aplicação e typescript configurado

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>ig.news - Home</title>
+      </Head>
+      <h1>Hello wooooooooorld!</h1>
+    </>
+  )
+}
